d0e547c509ac5c762f9d1395fb79a0c5cdc26e67
Riko <mr.rikowijayanto@gmail.com>
2020-06-26 22:31:04 +0700

    Add search bar

--- MyActionBar/app/src/main/java/com/example/myactionbar/MainActivity.java
+++ MyActionBar/app/src/main/java/com/example/myactionbar/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.myactionbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,20 +21,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public boolean onCreateOptionsMenu (Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.options_menu, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+
+        if (searchManager != null) {
+
+            SearchView searchView = (SearchView) (menu.findItem(R.id.search)).getActionView();
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+            searchView.setQueryHint(getResources().getString(R.string.search_hint));
+
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    return false;
+                }
+
+            });
+
+        }
+
         return true;
     }
 
-    public boolean onOptionsItemSelected (MenuItem item) {
+
+    public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
 
-            case R.id.menu1 :
+            case R.id.menu1:
                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MenuFragment()).addToBackStack(null).commit();
                 return true;
 
-            case R.id.menu2 :
+            case R.id.menu2:
                 Intent i = new Intent(MainActivity.this, MenuAcitivty.class);
                 startActivity(i);
                 return true;
