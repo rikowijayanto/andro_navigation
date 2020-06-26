bd79221a4bab192a3385aebdf5e92e21b351da54
Riko <mr.rikowijayanto@gmail.com>
2020-06-26 23:41:40 +0700

    refactor

--- MyNavigationDrawer/app/src/main/java/com/example/mynavigationdrawer/MainActivity.java
+++ MyNavigationDrawer/app/src/main/java/com/example/mynavigationdrawer/MainActivity.java
@@ -39,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
         //Floating menu
         FloatingActionButton fab = findViewById(R.id.fab);
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
